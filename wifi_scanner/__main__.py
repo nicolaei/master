11c91cd3d09c90f5270004b7e3abfac98bcbef50
Nicolas Harlem Eide <nicolaei@ifi.uio.no>
2020-02-29 16:13:53 +0100

    feat(wifi_scanner): Change to handle continious scanning and configurable setup

--- wifi_scanner/__main__.py
+++ wifi_scanner/__main__.py
@@ -1,24 +1,84 @@
+import json
 import logging
-from datetime import timedelta
+from os import path
+from json.decoder import JSONDecodeError
+from functools import partial
+from sys import argv
+from datetime import timedelta, datetime
 
-from wifi_scanner.algorithms import smooth_scan
-
+from wifi_scanner.algorithms import smooth_scan, full_scan, selective_scan
+from wifi_scanner.schedulers import random_trigger, interval_trigger, \
+    traffic_trigger, repeat_scan
 
 log = logging.getLogger(__name__)
 
 
-def main():
-    log.info("Starting smooth scan")
-    access_points = smooth_scan(timedelta(milliseconds=300))
+def write_output(file_name: str, access_points: list):
+    with open(file_name, "r") as json_file:
+        try:
+            data = json.load(json_file)
+        except JSONDecodeError as e:
+            # In this case the file is empty!
+            log.exception(e)
+            log.warning("The file was empty; initializing with empty list!")
+            data = []
 
-    log.info(f"Found {len(access_points)} access points!")
-    for ap in access_points:
-        print(ap)
+        data.append(
+            {
+                "time": datetime.now().timestamp(),
+                "aps": [
+                    {
+                        "bss": ap.bss,
+                        "ssid": ap.ssid,
+                        "frequency": ap.frequency,
+                        "signal_strength": ap.signal_strength
+                    }
+                    for ap in access_points
+                ]
+            },
+        )
 
+    with open(file_name, "w") as json_file:
+        json.dump(data, json_file)
+
+
+algorithms = {
+    "full": full_scan,
+    "selective": selective_scan,
+    "smooth": smooth_scan,
+}
+
+schedulers = {
+    "single": partial(repeat_scan, repetitions=1),
+    "5min": partial(interval_trigger, interval=timedelta(minutes=5)),
+    "traffic": traffic_trigger
+}
 
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG,
         format="[%(asctime)s][%(name)s][%(levelname)s]: %(message)s"
     )
-    main()
+
+    # This could use Argparse, but that's really overcomplicating this simple
+    # usecase.
+    if len(argv) < 3:
+        print(f"Not enough arguments! "
+              f"Usage: {argv[0]} <scanning algorithm> <scheduler>")
+        exit(1)
+    elif argv[1] not in algorithms.keys():
+        print(f"Invalid scanning algorithm. "
+              f"Choose from: {', '.join(algorithms.keys())}")
+    elif argv[2] not in algorithms.keys():
+        print(f"Invalid scheduler. "
+              f"Choose from: {', '.join(schedulers.keys())}")
+
+    algorithm = algorithms[argv[1]]
+    scheduler = schedulers[argv[2]]
+
+    log.info(f"Starting {argv[2]} with {argv[1]} scan")
+    for scan in scheduler(algorithm):
+        write_output(
+            path.expanduser("/tmp/scanner_measurements.json"),
+            list(scan)
+        )
