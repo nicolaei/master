f46486ebaccbc88b89f0d77c904e63e5e3c74cc4
Nicolas Harlem Eide <nicolaei@ifi.uio.no>
2020-03-05 14:14:13 +0100

    fixup! feat(wifi_scanner): Change to handle continious scanning and configurable setup

--- wifi_scanner/__main__.py
+++ wifi_scanner/__main__.py
@@ -14,7 +14,7 @@ log = logging.getLogger(__name__)
 
 
 def write_output(file_name: str, access_points: list):
-    with open(file_name, "r") as json_file:
+    with open(file_name, "r+") as json_file:
         try:
             data = json.load(json_file)
         except JSONDecodeError as e:
@@ -23,20 +23,20 @@ def write_output(file_name: str, access_points: list):
             log.warning("The file was empty; initializing with empty list!")
             data = []
 
-        data.append(
-            {
-                "time": datetime.now().timestamp(),
-                "aps": [
-                    {
-                        "bss": ap.bss,
-                        "ssid": ap.ssid,
-                        "frequency": ap.frequency,
-                        "signal_strength": ap.signal_strength
-                    }
-                    for ap in access_points
-                ]
-            },
-        )
+    data.append(
+        {
+            "time": datetime.now().timestamp(),
+            "aps": [
+                {
+                    "bss": ap.bss,
+                    "ssid": ap.ssid,
+                    "frequency": ap.frequency,
+                    "signal_strength": ap.signal_strength
+                }
+                for ap in access_points
+            ]
+        },
+    )
 
     with open(file_name, "w") as json_file:
         json.dump(data, json_file)
@@ -67,16 +67,18 @@ if __name__ == "__main__":
               f"Usage: {argv[0]} <scanning algorithm> <scheduler>")
         exit(1)
     elif argv[1] not in algorithms.keys():
-        print(f"Invalid scanning algorithm. "
+        print(f"Invalid scanning algorithm '{argv[1]}'. "
               f"Choose from: {', '.join(algorithms.keys())}")
-    elif argv[2] not in algorithms.keys():
-        print(f"Invalid scheduler. "
+        exit(1)
+    elif argv[2] not in schedulers.keys():
+        print(f"Invalid scheduler '{argv[2]}'. "
               f"Choose from: {', '.join(schedulers.keys())}")
+        exit(1)
 
     algorithm = algorithms[argv[1]]
     scheduler = schedulers[argv[2]]
 
-    log.info(f"Starting {argv[2]} with {argv[1]} scan")
+    log.info(f"===== Starting {argv[2]} with {argv[1]} scan =====")
     for scan in scheduler(algorithm):
         write_output(
             path.expanduser("~pi/scanner_measurements.json"),
