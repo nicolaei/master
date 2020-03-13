2c08de11524f0aed3dd88a4be2019430c5958e2e
Nicolas Harlem Eide <nicolaei@ifi.uio.no>
2020-03-13 16:13:39 +0100

    feat(graphs): Add a timeframe for rendering subsets of data

--- bin/graphs.py
+++ bin/graphs.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 
-def draw(folder: str):
+def draw(folder: str, limit_a: datetime, limit_b: datetime):
     time = []
     db = []
     latency = []
+
     with open(f"./measurements/{folder}/client_measurement.csv") as f:
         reader = csv.reader(f)
         for row in reader:
@@ -19,7 +20,7 @@ def draw(folder: str):
             latency.append(float(row[2]))
 
     with open(f"./measurements/{folder}/scanner_measurements.json") as f:
-        data = json.load(f)
+        ap_data = json.load(f)
 
     fig, ax1 = plt.subplots()
 
@@ -29,17 +30,14 @@ def draw(folder: str):
     ax1.plot(time, db, color=color)
     ax1.tick_params(axis='y', labelcolor=color)
 
-    color = 'tab:green'
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    color = 'tab:green'
     ax2.set_ylabel('Latency', color=color)  # we already handled the x-label with
     ax2.plot(time, latency, color=color)
     ax2.tick_params(axis='y', labelcolor=color)
 
 
-    ax1.set_xlim(
-        datetime(year=2020, month=3, day=7, hour=0, minute=0, second=0),
-        datetime(year=2020, month=3, day=7, hour=1, minute=0, second=0)
-    )
+    ax1.set_xlim(limit_a, limit_b)
     ax2.set_ylim(0, 5)
 
     ax1.xaxis.set_major_locator(mdates.MinuteLocator(interval=5))
@@ -48,11 +46,33 @@ def draw(folder: str):
     ax2.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
 
     [plt.axvline(x=datetime.fromtimestamp(float(data_point["time"])))
-     for data_point in data]
+     for data_point in ap_data]
 
     fig.set_size_inches(30, 10)
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
 
 
-draw("full_5min_far")
+draw(
+    "full_5min_far",
+    limit_a=datetime(year=2020, month=3, day=7, hour=0, minute=0, second=0),
+    limit_b=datetime(year=2020, month=3, day=7, hour=1, minute=0, second=0)
+)
+
+draw(
+    "full_5min_close",
+    limit_a=datetime(year=2020, month=3, day=8, hour=0, minute=0, second=0),
+    limit_b=datetime(year=2020, month=3, day=8, hour=1, minute=0, second=0)
+)
+
+draw(
+    "smooth_5min_far",
+    limit_a=datetime(year=2020, month=3, day=9, hour=0, minute=0, second=0),
+    limit_b=datetime(year=2020, month=3, day=9, hour=1, minute=0, second=0)
+)
+
+draw(
+    "smooth_5min_close",
+    limit_a=datetime(year=2020, month=3, day=12, hour=0, minute=0, second=0),
+    limit_b=datetime(year=2020, month=3, day=12, hour=1, minute=0, second=0)
+)
