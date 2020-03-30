93febeb9a962d4f10a9fede9886ba06ab1e7b579
Nicolas Harlem Eide <nicolaei@ifi.uio.no>
2020-03-30 20:22:59 +0200

    fix(client): Change ping call to get absolute value every 250 ms
    Using async just to test it out really

--- client_logger/__main__.py
+++ client_logger/__main__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Continiously monitor the quality of connection"""
+import asyncio
 import csv
 import logging
-import subprocess
+import re
 from datetime import datetime
 from os import path
-from time import sleep
 
 log = logging.getLogger(__name__)
 
@@ -25,34 +25,54 @@ def db_reading():
         return float(f.readlines()[2].split()[3])
 
 
-def latency(host: str):
-    """Get the latency from a single ping to host"""
-    output = subprocess.run(
-        ["ping", "-i", "1", "-c", "5", "-I", "wlan0", f"{host}"],
-        capture_output=True
-    ).stdout.decode()
+async def latency(
+    host: str,
+    interval: float = 0.25,
+    timeout: float = 0.25,
+    interface: str = "wlan0",
+):
+    """Get the latency to a host every interval
 
-    # This seemed like the easiest way to get the latency of the single ping.
-    # This grabs the "min" from the statistics on the last line of "ping".
-    # Lazy and stupid, but hey it's science.
-    return float(output.splitlines()[-1].split()[-2].split("/")[0])
+    :param host: The host to check against
+    :param interval: The amount of time between each ping request in seconds
+    :param timeout: The max amount of time to wait for a response in seconds.
+    :param interface: The interface to do the request on.
+    """
+    process = await asyncio.create_subprocess_shell(
+        f"ping -O -i {interval} -W {timeout} -I {interface} {host}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
 
+    # Ping always prints one info line. This line should be ignored.
+    await process.stdout.readline()
 
-def measure():
-    log.info("===== Starting data collection =====")
     while True:
-        sleep(1)
+        stdout = (await process.stdout.readline()).decode("utf-8")
+        if stdout.startswith("no answer yet for"):
+            yield None
+        else:
+            yield float(re.search(r"(?<=time=)\S+(?= ms)", stdout).group())
+
+
+async def measure():
+    log.info("===== Starting data collection =====")
+
+    async for ping in latency("192.168.4.1"):
         try:
-            write_output(
-                path.expanduser("~pi/client_measurement.csv"),
-                [db_reading(), latency("192.168.4.1")]
-            )
-        except IndexError as e:
+            signal_strength = db_reading()
+        except IndexError:
             log.warning(
                 "Measurement Error: Couldn't read dB or latency.\n"
                 "You're probably not connected to a network. This mostly "
                 "happens if you just booted your device."
             )
+            continue
+
+        write_output(
+            path.expanduser("~pi/client_measurement.csv"),
+            [signal_strength, ping]
+        )
 
 
 if __name__ == "__main__":
@@ -61,6 +81,6 @@ if __name__ == "__main__":
         format="[%(asctime)s][%(name)s][%(levelname)s]: %(message)s"
     )
     try:
-        measure()
+        asyncio.run(measure())
     except Exception as e:
         log.exception(e)
