beb06afd847a741710cd0f0ed51ef79060989533
Nicolas Harlem Eide <nicolaei@ifi.uio.no>
2020-05-01 10:39:57 +0200

    fix(troughput_tester): Revert the server back to before epoll
    Using epoll just created more error-handling. I don't need epoll anyways.

--- troughput_tester/server.py
+++ troughput_tester/server.py
@@ -1,28 +1,17 @@
 import logging
-import selectors
-import socket
+import socketserver
 
 logger = logging.getLogger(__name__)
 
 HOST = "0.0.0.0"
 PORT = 50000
-BUFFER_SIZE = 2 ** 13
 
 
-def server():
-    poller = selectors.DefaultSelector()
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((HOST, PORT))
-    sock.setblocking(False)
-
-    poller.register(sock, selectors.EVENT_READ)
-
-    while True:
-        for key, mask in poller.select(timeout=0.1):
-            if mask & selectors.EVENT_READ:
-                data, address = key.fileobj.recvfrom(BUFFER_SIZE)
-                key.fileobj.sendto(data, address)
+class TrughputServer(socketserver.BaseRequestHandler):
+    """We simply send the data back to the sender!"""
+    def handle(self):
+        data = self.request[0]
+        self.request[1].sendto(data, self.client_address)
 
 
 if __name__ == "__main__":
@@ -32,4 +21,6 @@ if __name__ == "__main__":
     )
 
     logger.info("Starting troughput server")
-    server()
+    with socketserver.UDPServer((HOST, PORT), TrughputServer) as server:
+        logger.info("Waiting for requests...")
+        server.serve_forever()
